8433e61bec7c88db979c2517f0377a18f7484693
磊哥 <493114008@qq.com>
2021-04-23 19:03:56 +0800

    添加 404 页面

---
+++ src/views/Notfount.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>当前页面不正确，请点击返回重新选择</div>
+    <div>
+    <router-link to="/">返回</router-link>|
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Notfount extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
