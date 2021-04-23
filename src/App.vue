92e74756be23c4e9d13e3db4525aacb460c65fb4
磊哥 <493114008@qq.com>
2021-04-23 18:50:35 +0800

    引入全局组件Nav

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,10 @@
 </template>
 
 <style lang="scss">
+@import "~@/assets/style/style.scss";
+
 #app {
+  background: $red;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
