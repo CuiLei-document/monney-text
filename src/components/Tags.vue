514cda624f2ce9fa10a1530f7abaa65533349078
磊哥 <493114008@qq.com>
2021-04-24 21:58:45 +0800

    把4个子组件收集到一个对象里 Record

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -26,6 +26,8 @@ export default class Tags extends Vue {
     }else{
       this.selectedTags.push(tag)
     }
+    this.$emit('update:value',this.selectedTags)
+
   }
   clear(){
     const name = window.prompt('请输入标签名')
@@ -35,6 +37,7 @@ export default class Tags extends Vue {
         this.$emit('update:dataSource',[...this.dataSource,name])
       }
     }
+
   }
 
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,10 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPads/>
-    <Types :xxx="333"/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
+    {{record}}
+    <NumberPads @update:value="onUpdateNumberPads"/>
+    <Types :value.sync="record.types" />
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
@@ -16,11 +17,32 @@ import Notes from '@/components/Notes.vue';
 import Types from '@/components/Types.vue';
 import NumberPads from '@/components/NumberPads.vue';
 
+type Record = {
+  tags: string[]
+  notes: string
+  types: string
+  amount: number
+}
 @Component({
   components: {NumberPads, Types, Notes, Tags, Nav}
 })
 export default class Money extends Vue {
-  tags:string[] = ['衣','食','住','行','赌博']
+  tags: string[] = ['衣', '食', '住', '行', '赌博'];
+  record: Record = {
+    tags: [], notes: '', types: '-', amount: 0
+  };
+
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
+  }
+
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
+
+  onUpdateNumberPads(value: string) {
+    this.record.amount = value;
+  }
 }
 </script>
 <style lang="scss">
