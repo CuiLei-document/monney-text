52ca3e1edfb28f834b60d373b3bdfeb14b5eb439
磊哥 <493114008@qq.com>
2021-04-24 12:39:12 +0800

    模块化：把Money.vue 分成4个小组件

---
+++ src/components/Notes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里输入备注">
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+@Component
+export default class Notes extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    line-height: 62px;
+    flex-grow: 1;
+    border: none;
+    background: transparent;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Tags.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+@Component
+export default class Tags extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  padding: 16px;
+  font-size: 14px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+
+  > .current {
+    display: flex;
+    flex-wrap:wrap;
+    > li {
+      background: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+      line-height: $h;
+      margin-top: 6px;
+    }
+  }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid;
+      color: #999;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
