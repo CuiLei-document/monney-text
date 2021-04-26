b1f26f47a64435c93914cf4a1f3f8d9c2c641aa7
磊哥 <493114008@qq.com>
2021-04-26 19:28:55 +0800

    封装 全局 createTag Labels 来引用全局 createTag

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,18 @@ Vue.component('Nav', Nav);
 Vue.component('Layout',Layout)
 Vue.component('Icon',Icon)
 window.tagList = tagListModel.fetch()
-
+window.createTag = (name:string)=>{
+    if(name){
+        const message = tagListModel.create(name)
+        console.log(tagListModel.create);
+        console.log(message);
+        if(message === 'duplicated'){
+            window.alert('失败了')
+        }else if(message === 'success'){
+            window.alert('成功了')
+        }
+    }
+}
 new Vue({
     router,
     store,
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -28,14 +28,7 @@ export default class Labels extends Vue {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   createTag(){
     const name = window.prompt('请输入标签名')
-    if(name){
-      const message = tagListModel.create(name)
-      if(message === 'duplicated'){
-        window.alert('失败了')
-      }else if(message === 'success'){
-        window.alert('成功了')
-      }
-    }
+    window.createTag(name)
   }
 }
 </script>
